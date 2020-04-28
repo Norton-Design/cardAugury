66298eed13ad1f8030e4905468f20a4f718726f9
michael_norton <nortonkmichael@gmail.com>
2020-04-27 20:34:49 -0400

    Fix card searcher function

--- src/scripts/card_searcher.js
+++ src/scripts/card_searcher.js
@@ -1,15 +1,19 @@
 export const cardSearcher = e => {
   e.preventDefault();
   const inputStr = document.getElementById("searchbar").value;
-  // console.log(inputStr);
   const url = 'https://api.scryfall.com/cards/named?fuzzy=';
   const searchUrl = url + validSearchInput(inputStr);
   // // send ES6 fetch request 
   return fetch(searchUrl)
-  //   // .then(data => data.json())
-    .then(res => console.log(res))
+    .then(res => {
+      const promise = res.json();
+      promise.then(value => {
+        console.log(value)
+        return value;
+      })
+
+    })
     .catch(err => console.log("ERROR"))
-  // return JSON object of search result
 }
 
 export const validSearchInput = searchInput => ( searchInput.split(' ').join("-") )
